15bd3853c6f1d77d176c2cae0af3d0c47c85703d
JavaCCGithub <1101898397@qq,com>
2019-03-03 23:25:16 +0800

    third test

--- src/main/java/com/cc/demo/pojo/Video.java
+++ src/main/java/com/cc/demo/pojo/Video.java
@@ -5,5 +5,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class Video {
 
+	private String title;
+	private int isbn;
+	private String auther;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	public String getAuther() {
+		return auther;
+	}
+	public void setAuther(String auther) {
+		this.auther = auther;
+	}
+	
 
 }
